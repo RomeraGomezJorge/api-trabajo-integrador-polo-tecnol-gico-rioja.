e64160f004db81e1f23f9cccfea7204b421c0ece
Romera Jorge <romera.gomez.jorge@gmail.com>
2023-12-20 17:26:59 -0300

    Install and config CORS

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ import dotenv from 'dotenv';
 import express from 'express';
 import { router } from './routes.js';
 import {connectMongoDB} from "./db/mongo.js";
+import cors from "cors";
 
 dotenv.config();
 connectMongoDB()
 const app = express();
 const port = process.env.PORT;
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use('/api/location', router);
